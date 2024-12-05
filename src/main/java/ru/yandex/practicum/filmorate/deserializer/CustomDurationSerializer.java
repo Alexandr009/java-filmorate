6662d5dca539016134f2dce7797d4a57469e1bd6
Aleksandr Abramovich <hause_abramovi44@mail.ru>
2024-12-06 00:10:26 +0300

    - fix checkstyle deserializer

--- src/main/java/ru/yandex/practicum/filmorate/deserializer/CustomDurationSerializer.java
+++ src/main/java/ru/yandex/practicum/filmorate/deserializer/CustomDurationSerializer.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.deserializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
